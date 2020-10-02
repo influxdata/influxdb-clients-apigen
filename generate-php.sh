33980da9bbdf3c6eafeccffa73a0fb7826c9411b
Robert Hajek <robert.hajek@gmail.com>
2020-10-02 10:43:39 +0200

    (feat) cleanup script

--- generate-php.sh
+++ generate-php.sh
@@ -4,22 +4,22 @@ set -ex
 
 SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-	rm -rf ./build/influxdb-client-php/generated
-	mvn -f ./openapi-generator/pom-php.xml -DswaggerLocation=./swagger.yml org.openapitools:openapi-generator-maven-plugin:generate
-	#### sync generated php files to src
+rm -rf ./build/influxdb-client-php/generated
+mvn -f ./openapi-generator/pom-php.xml -DswaggerLocation=./swagger.yml org.openapitools:openapi-generator-maven-plugin:generate
+#### sync generated php files to src
 
-	# delete old sources
-	rm -f ./build/influxdb-client-php/src/InfluxDB2/Service/*
-	rm -f ./build/influxdb-client-php/src/InfluxDB2/Model/*
+# delete old sources
+rm -f ./build/influxdb-client-php/src/InfluxDB2/Service/*
+rm -f ./build/influxdb-client-php/src/InfluxDB2/Model/*
 
-	#cp -r ./build/influxdb-client-php/generated/lib/ApiException.php ./build/influxdb-client-php/src/InfluxDB2
-	cp -r ./build/influxdb-client-php/generated/lib/ObjectSerializer.php ./build/influxdb-client-php/src/InfluxDB2
-	cp -r ./build/influxdb-client-php/generated/lib/HeaderSelector.php ./build/influxdb-client-php/src/InfluxDB2
+#cp -r ./build/influxdb-client-php/generated/lib/ApiException.php ./build/influxdb-client-php/src/InfluxDB2
+cp -r ./build/influxdb-client-php/generated/lib/ObjectSerializer.php ./build/influxdb-client-php/src/InfluxDB2
+cp -r ./build/influxdb-client-php/generated/lib/HeaderSelector.php ./build/influxdb-client-php/src/InfluxDB2
 
-	mkdir -p ./build/influxdb-client-php/src/InfluxDB2/Model
-	mkdir -p ./build/influxdb-client-php/src/InfluxDB2/Service
+mkdir -p ./build/influxdb-client-php/src/InfluxDB2/Model
+mkdir -p ./build/influxdb-client-php/src/InfluxDB2/Service
 
-  cp -r ./build/influxdb-client-php/generated/lib/Service/*.php ./build/influxdb-client-php/src/InfluxDB2/Service
-  cp -r ./build/influxdb-client-php/generated/lib/Model/*.php ./build/influxdb-client-php/src/InfluxDB2/Model
+cp -r ./build/influxdb-client-php/generated/lib/Service/*.php ./build/influxdb-client-php/src/InfluxDB2/Service
+cp -r ./build/influxdb-client-php/generated/lib/Model/*.php ./build/influxdb-client-php/src/InfluxDB2/Model
 
-	#rm -rf ./build/influxdb-client-php/generated
+#rm -rf ./build/influxdb-client-php/generated
