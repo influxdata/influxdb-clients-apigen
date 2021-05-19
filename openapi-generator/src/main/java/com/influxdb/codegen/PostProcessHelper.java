06dc287febc1c7a04ca193713de3f3cc2e3687f8
Jakub Bednar <jakub.bednar@gmail.com>
2021-05-19 10:21:42 +0200

    feat(python): prepare to generate API from oss

---
+++ openapi-generator/src/main/java/com/influxdb/codegen/PostProcessHelper.java
@@ -0,0 +1,78 @@
+package com.influxdb.codegen;
+
+import javax.annotation.Nonnull;
+
+import java.util.Map;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.media.ComposedSchema;
+import io.swagger.v3.oas.models.media.DateTimeSchema;
+import io.swagger.v3.oas.models.media.MediaType;
+import io.swagger.v3.oas.models.media.ObjectSchema;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.parameters.RequestBody;
+import org.openapitools.codegen.CodegenOperation;
+
+/**
+ * @author Jakub Bednar (18/05/2021 13:20)
+ */
+class PostProcessHelper
+{
+	private final OpenAPI openAPI;
+
+	public PostProcessHelper(final OpenAPI openAPI)
+	{
+		this.openAPI = openAPI;
+	}
+
+	void postProcessOpenAPI()
+	{
+		//
+		// Drop supports for InfluxQL
+		//
+		{
+			RequestBody requestBody = openAPI.getPaths().get("/query").getPost().getRequestBody();
+			MediaType mediaType = requestBody.getContent().get("application/json");
+			// Set correct schema to `Query` object
+			Schema schema = ((ComposedSchema) mediaType.getSchema()).getOneOf().get(0);
+			mediaType.schema(schema);
+		}
+
+		//
+		// Set DateTimeSchema for DateTimeLiteral
+		//
+		{
+			ObjectSchema objectSchema = (ObjectSchema) openAPI.getComponents().getSchemas().get("DateTimeLiteral");
+			Map<String, Schema> properties = objectSchema.getProperties();
+			Schema dateTimeSchema = new DateTimeSchema()
+					.type("string")
+					.format("date-time")
+					.description(properties.get("value").getDescription());
+			properties.put("value", dateTimeSchema);
+		}
+	}
+
+	void postProcessOperation(String path, Operation operation, CodegenOperation op)
+	{
+		//
+		// Set correct path for /health, /ready, /setup ...
+		//
+		String url;
+		if (operation.getServers() != null) {
+			url = operation.getServers().get(0).getUrl();
+		} else if (openAPI.getPaths().get(path).getServers() != null) {
+			url = openAPI.getPaths().get(path).getServers().get(0).getUrl();
+		} else {
+			url = openAPI.getServers().get(0).getUrl();
+		}
+
+		if (url != null) {
+			url = url.replaceAll("https://raw.githubusercontent.com", "");
+		}
+
+		if (!"/".equals(url) && url != null) {
+			op.path = url + op.path;
+		}
+	}
+}
