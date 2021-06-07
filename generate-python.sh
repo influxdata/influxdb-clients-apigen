4ce04b245457444450fe528abc222257aea19d52
Jakub Bednar <jakub.bednar@gmail.com>
2021-06-07 13:58:08 +0200

    chore(python): version of client is specified on one place

--- generate-python.sh
+++ generate-python.sh
@@ -7,4 +7,4 @@ SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 rm ./build/influxdb-client-python/influxdb_client/domain/*.py || true
 rm ./build/influxdb-client-python/influxdb_client/service/*.py || true
 
-mvn -f ./openapi-generator/pom-python.xml -DclientVersion=1.11.0dev -DswaggerLocation="${SCRIPT_PATH}/oss.yml" org.openapitools:openapi-generator-maven-plugin:generate
+mvn -f ./openapi-generator/pom-python.xml -DswaggerLocation="${SCRIPT_PATH}/oss.yml" org.openapitools:openapi-generator-maven-plugin:generate
