eac62d406e05230677722744041a6c4c639181c1
Jakub Bednar <jakub.bednar@gmail.com>
2021-10-25 09:17:21 +0200

    fix(java): add possibility to use git patch to deal with breaking changes

--- generate-java.sh
+++ generate-java.sh
@@ -7,4 +7,8 @@ SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 rm -f ./build/influxdb-client-java/client/src/generated/java/com/influxdb/client/domain/*.java
 rm -f ./build/influxdb-client-java/client/src/generated/java/com/influxdb/client/service/*.java
 
+if [ -f "$SCRIPT_PATH"/influxdb-client-java.patch ]; then
+  (cd "$SCRIPT_PATH"/build/influxdb-client-java && git apply "$SCRIPT_PATH"/influxdb-client-java.patch)
+fi
+
 mvn -f ./openapi-generator/pom-java.xml -DswaggerLocation="${SCRIPT_PATH}/oss.yml" org.openapitools:openapi-generator-maven-plugin:generate
