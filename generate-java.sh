efd185764fe9992ada0f49629197c91eee587ae0
Jakub Bednář <jakub.bednar@gmail.com>
2021-10-25 09:30:26 +0200

    fix(java): add possibility to use git patch to deal with breaking changes (#30)

--- generate-java.sh
+++ generate-java.sh
@@ -4,6 +4,10 @@ set -ex
 
 SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
+if [ -f "$SCRIPT_PATH"/influxdb-client-java.patch ]; then
+  (cd "$SCRIPT_PATH"/build/influxdb-client-java/ && git apply "$SCRIPT_PATH"/influxdb-client-java.patch)
+fi
+
 rm -f ./build/influxdb-client-java/client/src/generated/java/com/influxdb/client/domain/*.java
 rm -f ./build/influxdb-client-java/client/src/generated/java/com/influxdb/client/service/*.java
 
